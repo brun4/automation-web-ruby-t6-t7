05a7ab33d565ab115f5858be3d36729ae92ee88b
brun4 <bruna.dias.silva@usp.br>
2022-08-12 00:08:53 -0300

    Config environment and cucumber yml files

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,13 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'pry'
 
+ENVIRONMENT = ENV['ENVIRONMENT']
+puts "ENVIRONMENT is running >>> #{ENVIRONMENT}"
+
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}.yml")
+World(Helper)
+World(Pages)
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = CONFIG['url_default']
